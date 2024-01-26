37027131368a41896a2edee3dcd871cda3f3e1a8
Lance <71721648+slicer229@users.noreply.github.com>
2024-01-26 12:19:07 -0700

    added frmDatabaseViewer.cs

---
+++ Team3Workshop4/frmDatabaseViewer.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Team3Workshop4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPackages = new TabPage();
+            tabProducts = new TabPage();
+            tabProdSupp = new TabPage();
+            tabSuppliers = new TabPage();
+            tabPackProdSupp = new TabPage();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            dataGridView3 = new DataGridView();
+            dataGridView4 = new DataGridView();
+            dataGridView5 = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPackages.SuspendLayout();
+            tabProducts.SuspendLayout();
+            tabProdSupp.SuspendLayout();
+            tabSuppliers.SuspendLayout();
+            tabPackProdSupp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView5).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPackages);
+            tabControl1.Controls.Add(tabProducts);
+            tabControl1.Controls.Add(tabProdSupp);
+            tabControl1.Controls.Add(tabSuppliers);
+            tabControl1.Controls.Add(tabPackProdSupp);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(821, 532);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPackages
+            // 
+            tabPackages.Controls.Add(dataGridView1);
+            tabPackages.Location = new Point(4, 24);
+            tabPackages.Name = "tabPackages";
+            tabPackages.Padding = new Padding(3);
+            tabPackages.Size = new Size(813, 504);
+            tabPackages.TabIndex = 0;
+            tabPackages.Text = "Packages";
+            tabPackages.UseVisualStyleBackColor = true;
+            // 
+            // tabProducts
+            // 
+            tabProducts.Controls.Add(dataGridView2);
+            tabProducts.Location = new Point(4, 24);
+            tabProducts.Name = "tabProducts";
+            tabProducts.Padding = new Padding(3);
+            tabProducts.Size = new Size(813, 504);
+            tabProducts.TabIndex = 1;
+            tabProducts.Text = "Products";
+            tabProducts.UseVisualStyleBackColor = true;
+            // 
+            // tabProdSupp
+            // 
+            tabProdSupp.Controls.Add(dataGridView3);
+            tabProdSupp.Location = new Point(4, 24);
+            tabProdSupp.Name = "tabProdSupp";
+            tabProdSupp.Size = new Size(813, 504);
+            tabProdSupp.TabIndex = 2;
+            tabProdSupp.Text = "Products_suppliers";
+            tabProdSupp.UseVisualStyleBackColor = true;
+            // 
+            // tabSuppliers
+            // 
+            tabSuppliers.Controls.Add(dataGridView4);
+            tabSuppliers.Location = new Point(4, 24);
+            tabSuppliers.Name = "tabSuppliers";
+            tabSuppliers.Size = new Size(813, 504);
+            tabSuppliers.TabIndex = 3;
+            tabSuppliers.Text = "Suppliers";
+            tabSuppliers.UseVisualStyleBackColor = true;
+            // 
+            // tabPackProdSupp
+            // 
+            tabPackProdSupp.Controls.Add(dataGridView5);
+            tabPackProdSupp.Location = new Point(4, 24);
+            tabPackProdSupp.Name = "tabPackProdSupp";
+            tabPackProdSupp.Size = new Size(813, 504);
+            tabPackProdSupp.TabIndex = 4;
+            tabPackProdSupp.Text = "Packages_products_suppliers";
+            tabPackProdSupp.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(813, 455);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(0, 0);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(813, 455);
+            dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(0, 0);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(813, 455);
+            dataGridView3.TabIndex = 1;
+            // 
+            // dataGridView4
+            // 
+            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView4.Location = new Point(0, 0);
+            dataGridView4.Name = "dataGridView4";
+            dataGridView4.Size = new Size(813, 455);
+            dataGridView4.TabIndex = 1;
+            // 
+            // dataGridView5
+            // 
+            dataGridView5.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView5.Location = new Point(0, 0);
+            dataGridView5.Name = "dataGridView5";
+            dataGridView5.Size = new Size(813, 455);
+            dataGridView5.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(845, 556);
+            Controls.Add(tabControl1);
+            Name = "MainForm";
+            Text = "Database Viewer";
+            tabControl1.ResumeLayout(false);
+            tabPackages.ResumeLayout(false);
+            tabProducts.ResumeLayout(false);
+            tabProdSupp.ResumeLayout(false);
+            tabSuppliers.ResumeLayout(false);
+            tabPackProdSupp.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView5).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPackages;
+        private TabPage tabProducts;
+        private TabPage tabProdSupp;
+        private TabPage tabSuppliers;
+        private TabPage tabPackProdSupp;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView3;
+        private DataGridView dataGridView4;
+        private DataGridView dataGridView5;
+    }
+}
