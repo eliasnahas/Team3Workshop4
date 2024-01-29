5be524cf428b3b268fe3904da1c7c84333e16530
Lance <71721648+slicer229@users.noreply.github.com>
2024-01-29 09:21:38 -0700

    cache commit

--- TravelExpertsData/obj/Debug/net8.0/TravelExpertsData.AssemblyInfo.cs
+++ TravelExpertsData/obj/Debug/net8.0/TravelExpertsData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelExpertsData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1955a43dc81831488ba5b854a4ce1b6463bccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5950a17a91490ea12563f0e2f070c4ac31916d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelExpertsData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelExpertsData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
